78af223bf66dab98fba5548bb212188c36fa5fd2
Bruce Wayne <admin@bige0.com>
2020-10-22 16:11:41 +0800

    Refine

--- SingleInstance/SingleInstance.cs
+++ SingleInstance/SingleInstance.cs
@@ -7,149 +7,139 @@ using System.Threading;
 
 namespace SingleInstance
 {
-    /// <summary>
-    /// Enforces single instance for an application.
-    /// </summary>
-    public sealed class SingleInstance : IDisposable
-    {
-        private Mutex _mutex;
-        private readonly bool _ownsMutex;
-        private readonly string _identifier;
+	/// <summary>
+	/// Enforces single instance for an application.
+	/// </summary>
+	public sealed class SingleInstance : IDisposable
+	{
+		private Mutex _mutex;
+		private readonly bool _ownsMutex;
+		private readonly string _identifier;
 
-        /// <summary>
-        /// Enforces single instance for an application.
-        /// </summary>
-        /// <param name="identifier">An identifier unique to this application.</param>
-        public SingleInstance(string identifier)
-        {
-            _identifier = identifier;
-            _mutex = new Mutex(true, identifier, out _ownsMutex);
-        }
+		/// <summary>
+		/// Enforces single instance for an application.
+		/// </summary>
+		/// <param name="identifier">An identifier unique to this application.</param>
+		public SingleInstance(string identifier)
+		{
+			_identifier = identifier;
+			_mutex = new Mutex(true, identifier, out _ownsMutex);
+		}
 
-        /// <summary>
-        /// Indicates whether this is the first instance of this application.
-        /// </summary>
-        public bool IsFirstInstance => _ownsMutex;
+		/// <summary>
+		/// Indicates whether this is the first instance of this application.
+		/// </summary>
+		public bool IsFirstInstance => _ownsMutex;
 
-        /// <summary>
-        /// Passes the given arguments to the first running instance of the application.
-        /// </summary>
-        /// <param name="arguments">The arguments to pass.</param>
-        /// <returns>Return true if the operation succeeded, false otherwise.</returns>
-        public bool PassArgumentsToFirstInstance(IEnumerable<string> arguments)
-        {
-            if (IsFirstInstance)
-                throw new InvalidOperationException("This is the first instance.");
+		/// <summary>
+		/// Passes the given arguments to the first running instance of the application.
+		/// </summary>
+		/// <param name="arguments">The arguments to pass.</param>
+		/// <returns>Return true if the operation succeeded, false otherwise.</returns>
+		public bool PassArgumentsToFirstInstance(IEnumerable<string> arguments)
+		{
+			if (IsFirstInstance)
+			{
+				throw new InvalidOperationException(@"This is the first instance.");
+			}
 
-            try
-            {
-                using (var client = new NamedPipeClientStream(_identifier))
-                using (var writer = new StreamWriter(client))
-                {
-                    client.Connect(200);
+			try
+			{
+				using var client = new NamedPipeClientStream(_identifier);
+				using var writer = new StreamWriter(client);
 
-                    foreach (var argument in arguments)
-                        writer.WriteLine(argument);
-                }
-                return true;
-            }
-            catch (TimeoutException)
-            { } //Couldn't connect to server
-            catch (IOException)
-            { } //Pipe was broken
+				client.Connect(200);
 
-            return false;
-        }
+				foreach (var argument in arguments)
+				{
+					writer.WriteLine(argument);
+				}
 
-        /// <summary>
-        /// Listens for arguments being passed from successive instances of the application.
-        /// </summary>
-        public void ListenForArgumentsFromSuccessiveInstances()
-        {
-            if (!IsFirstInstance)
-                throw new InvalidOperationException("This is not the first instance.");
-            ThreadPool.QueueUserWorkItem(ListenForArguments);
-        }
+				return true;
+			}
+			catch (TimeoutException)
+			{
+				//Couldn't connect to server
+			}
+			catch (IOException)
+			{
+				//Pipe was broken
+			}
 
-        /// <summary>
-        /// Listens for arguments on a named pipe.
-        /// </summary>
-        /// <param name="state">State object required by WaitCallback delegate.</param>
-        // ReSharper disable once FunctionRecursiveOnAllPaths
-        private void ListenForArguments(object state)
-        {
-            try
-            {
-                using var server = new NamedPipeServerStream(_identifier);
-                using var reader = new StreamReader(server);
-                server.WaitForConnection();
+			return false;
+		}
 
-                var arguments = new List<string>();
-                while (server.IsConnected)
-                    arguments.Add(reader.ReadLine());
+		/// <summary>
+		/// Listens for arguments being passed from successive instances of the application.
+		/// </summary>
+		public void ListenForArgumentsFromSuccessiveInstances()
+		{
+			if (!IsFirstInstance)
+			{
+				throw new InvalidOperationException(@"This is not the first instance.");
+			}
 
-                ThreadPool.QueueUserWorkItem(CallOnArgumentsReceived, arguments.ToArray());
-            }
-            catch (IOException)
-            { } //Pipe was broken
-            finally
-            {
-                ListenForArguments(null);
-            }
-        }
+			ThreadPool.QueueUserWorkItem(ListenForArguments);
+		}
 
-        /// <summary>
-        /// Calls the OnArgumentsReceived method casting the state Object to String[].
-        /// </summary>
-        /// <param name="state">The arguments to pass.</param>
-        private void CallOnArgumentsReceived(object state)
-        {
-            OnArgumentsReceived((string[])state);
-        }
+		/// <summary>
+		/// Listens for arguments on a named pipe.
+		/// </summary>
+		/// <param name="state">State object required by WaitCallback delegate.</param>
+		// ReSharper disable once FunctionRecursiveOnAllPaths
+		private void ListenForArguments(object state)
+		{
+			try
+			{
+				using var server = new NamedPipeServerStream(_identifier);
+				using var reader = new StreamReader(server);
+				server.WaitForConnection();
 
-        /// <summary>
-        /// Event raised when arguments are received from successive instances.
-        /// </summary>
-        private readonly Subject<string[]> _argumentsReceived = new Subject<string[]>();
-        public IObservable<string[]> ArgumentsReceived => _argumentsReceived;
+				var arguments = new List<string>();
+				while (server.IsConnected)
+				{
+					arguments.Add(reader.ReadLine());
+				}
 
-        /// <summary>
-        /// Fires the ArgumentsReceived event.
-        /// </summary>
-        /// <param name="arguments">The arguments to pass with the ArgumentsReceivedEventArgs.</param>
-        private void OnArgumentsReceived(string[] arguments)
-        {
-            _argumentsReceived.OnNext(arguments);
-        }
+				ThreadPool.QueueUserWorkItem(OnArgumentsReceived, arguments.ToArray());
+			}
+			catch (IOException)
+			{
+				//Pipe was broken
+			}
+			finally
+			{
+				ListenForArguments(null);
+			}
+		}
 
-        #region IDisposable
-        private bool _disposed;
+		private readonly Subject<string[]> _argumentsReceived = new Subject<string[]>();
+		public IObservable<string[]> ArgumentsReceived => _argumentsReceived;
 
-        // ReSharper disable once UnusedParameter.Local
-        private void Dispose(bool disposing)
-        {
-            if (!_disposed)
-            {
-                if (_mutex != null && _ownsMutex)
-                {
-                    _mutex.ReleaseMutex();
-                    _mutex = null;
-                }
-                _argumentsReceived.OnCompleted();
-               _disposed = true;
-            }
-        }
+		private void OnArgumentsReceived(object arguments)
+		{
+			_argumentsReceived.OnNext((string[])arguments);
+		}
 
-        ~SingleInstance()
-        {
-            Dispose(false);
-        }
+		#region IDisposable
+		private volatile bool _disposed;
 
-        public void Dispose()
-        {
-            Dispose(true);
-            GC.SuppressFinalize(this);
-        }
-        #endregion
-    }
+		public void Dispose()
+		{
+			if (_disposed)
+			{
+				return;
+			}
+			_disposed = true;
+
+			if (_mutex != null && _ownsMutex)
+			{
+				_mutex.ReleaseMutex();
+				_mutex = null;
+			}
+
+			_argumentsReceived.OnCompleted();
+		}
+		#endregion
+	}
 }
